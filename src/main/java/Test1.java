805b54e908a202b6687dd73660696a89adf9f52b
lovepatel2003 <lloovveeppaatteell@gmail.com>
2020-06-06 21:25:23 -0400

    day4_version_1

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -1,4 +1,4 @@
-import org.openqa.selenium.By;
+/*import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -32,4 +32,4 @@ public class Test1 {
         //driver.close();
 
     }
-}
+}*/
